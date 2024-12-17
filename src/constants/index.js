24400ed36fee1322e233e9c3eb5a5ad334562d90
DevAhmedKhalil <dev.akhalil01@gmail.com>
2024-12-17 15:38:43 +0200

    make ak-portfolio-website

---
+++ src/constants/index.js
@@ -0,0 +1,203 @@
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+export const services = [
+  {
+    title: "JavaScript",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-javascript-logo-icon-download-in-svg-png-gif-file-formats--brand-company-business-brands-pack-logos-icons-2284965.png?f=webp&w=256",
+  },
+  {
+    title: "Node JS",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-nodejs-logo-icon-download-in-svg-png-gif-file-formats--brand-development-tools-pack-logos-icons-226034.png?f=webp&w=256",
+  },
+  {
+    title: "TypeScript",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-typescript-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-vol-7-pack-logos-icons-3030260.png?f=webp&w=256",
+  },
+  {
+    title: "React JS",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-react-logo-icon-download-in-svg-png-gif-file-formats--programming-langugae-freebies-pack-logos-icons-1175109.png?f=webp&w=256",
+  },
+  {
+    title: "Java",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-java-logo-icon-download-in-svg-png-gif-file-formats--wordmark-programming-language-pack-logos-icons-1174953.png?f=webp&w=256",
+  },
+  {
+    title: "C++",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-cplusplus-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-company-brand-vol-2-pack-logos-icons-3029933.png?f=webp&w=256",
+  },
+  {
+    title: "Python",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-python-logo-icon-download-in-svg-png-gif-file-formats--brand-development-tools-pack-logos-icons-226051.png?f=webp&w=256",
+  },
+  {
+    title: "PhP",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-php-logo-icon-download-in-svg-png-gif-file-formats--brand-company-business-brands-pack-logos-icons-2284918.png?f=webp&w=256",
+  },
+];
+
+export const technologies = [
+  {
+    name: "HTML 5",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-html-logo-icon-download-in-svg-png-gif-file-formats--brand-development-tools-pack-logos-icons-225995.png?f=webp&w=256",
+  },
+  {
+    name: "CSS 3",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-css-logo-icon-download-in-svg-png-gif-file-formats--brand-development-tools-pack-logos-icons-226095.png?f=webp&w=256",
+  },
+  {
+    name: "JavaScript",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-javascript-logo-icon-download-in-svg-png-gif-file-formats--brand-company-business-brands-pack-logos-icons-2284965.png?f=webp&w=256",
+  },
+  {
+    name: "React JS",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-react-logo-icon-download-in-svg-png-gif-file-formats--programming-langugae-freebies-pack-logos-icons-1175109.png?f=webp&w=256",
+  },
+  {
+    name: "Tailwind CSS",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/512px-Tailwind_CSS_Logo.svg.png?20230715030042",
+  },
+  {
+    name: "Node JS",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-nodejs-logo-icon-download-in-svg-png-gif-file-formats--brand-development-tools-pack-logos-icons-226034.png?f=webp&w=256",
+  },
+  {
+    name: "Three JS",
+    icon: "https://global.discourse-cdn.com/flex035/uploads/threejs/optimized/2X/e/e4f86d2200d2d35c30f7b1494e96b9595ebc2751_2_496x500.png",
+  },
+  {
+    name: "git",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-git-logo-icon-download-in-svg-png-gif-file-formats--programming-langugae-language-pack-logos-icons-1175218.png?f=webp&w=256",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Backend Developer - E-commerce API",
+    company_name: "Self-Initiated Project",
+    icon: "https://cdn.iconscout.com/icon/premium/png-512-thumb/coding-development-plan-device-computer-programming-develop-code-38390.png?f=webp&w=256", // Backend-related icon
+    iconBg: "#161329",
+    date: "",
+    points: [
+      "Designed and developed a RESTful API for an e-commerce platform using Node.js and Express.",
+      "Implemented robust authentication and authorization using JWT and bcrypt for secure user access.",
+      "Integrated MongoDB for efficient data storage and retrieval, with optimized queries for product and user management.",
+      "Added custom validation using middleware to ensure data integrity across API endpoints.",
+    ],
+  },
+  {
+    title: "Category Management System - Backend Project",
+    company_name: "Self-Initiated Project",
+    icon: "https://cdn-icons-png.flaticon.com/512/3346/3346712.png",
+    iconBg: "#161329",
+    date: "",
+    points: [
+      "Developed a category management system with CRUD operations using Node.js and Express.",
+      "Processed image uploads with Multer and optimized image sizes using Sharp for better performance.",
+      "Implemented slug generation for dynamic URLs based on category names.",
+      "Designed a scalable architecture to handle future expansion, such as subcategories and product mapping.",
+    ],
+  },
+  {
+    title: "Learning Frontend Development",
+    company_name: "Personal Growth",
+    icon: "https://cdn-icons-png.flaticon.com/512/1055/1055687.png", // Learning-related icon
+    iconBg: "#161329",
+    date: "",
+    points: [
+      "Building an interactive portfolio using React, React Bootstrap, and Framer Motion for animations.",
+      "Learning to manage state effectively with React hooks like `useState` and `useEffect`.",
+      "Practicing modern CSS techniques, including responsive design and grid/flexbox layouts.",
+      "Integrating backend APIs with frontend components to create full-stack applications.",
+    ],
+  },
+];
+
+export const projects = [
+  {
+    name: "Courses App (Node.js)",
+    description:
+      "A course management system featuring CRUD operations with enhanced data storage using MongoDB. Includes role-based access control for Admin, Manager, and User roles.",
+    tags: [
+      { name: "Node.js", color: "blue-text-gradient" },
+      { name: "MongoDB", color: "green-text-gradient" },
+      { name: "Express.js", color: "pink-text-gradient" },
+    ],
+    image: "https://philna.sh/_astro/node.DvAuachI_1ovMWA.webp",
+    source_code_link: "https://github.com/DevAhmedKhalil/courses-app-nodejs",
+  },
+  {
+    name: "E-Commerce API (Node.js)",
+    description:
+      "A comprehensive API for managing an e-commerce platform, including features like user authentication, product management, and order handling.",
+    tags: [
+      { name: "Node.js", color: "blue-text-gradient" },
+      { name: "Express.js", color: "green-text-gradient" },
+      { name: "MongoDB", color: "pink-text-gradient" },
+    ],
+    image: "https://philna.sh/_astro/node.DvAuachI_1ovMWA.webp",
+    source_code_link: "https://github.com/DevAhmedKhalil/nodejs-ecommerce-api",
+  },
+  {
+    name: "Natours App",
+    description:
+      "A tour booking application providing user authentication, tour reviews, and booking functionality, with advanced controllers and optimized models.",
+    tags: [
+      { name: "Node.js", color: "blue-text-gradient" },
+      { name: "Express.js", color: "green-text-gradient" },
+      { name: "MongoDB", color: "pink-text-gradient" },
+    ],
+    image: "https://philna.sh/_astro/node.DvAuachI_1ovMWA.webp",
+    source_code_link: "https://github.com/DevAhmedKhalil/natours-app",
+  },
+  {
+    name: "Realtime Chat App",
+    description:
+      "A feature-rich real-time chat application with user authentication, group chats, file attachments, and responsive design.",
+    tags: [
+      { name: "Node.js", color: "blue-text-gradient" },
+      { name: "React.js", color: "green-text-gradient" },
+      { name: "ChatEngine.io", color: "pink-text-gradient" },
+    ],
+    image:
+      "https://www.logisticinfotech.com/wp-content/uploads/2018/08/Node-React-Js-1.png",
+    source_code_link: "https://github.com/DevAhmedKhalil/fullstack-chat",
+  },
+  {
+    name: "E-Commerce Django API",
+    description:
+      "A Django-based API for managing e-commerce platforms, featuring promotions, collections, customer management, and order handling.",
+    tags: [
+      { name: "Django", color: "blue-text-gradient" },
+      { name: "REST Framework", color: "green-text-gradient" },
+      { name: "Python", color: "pink-text-gradient" },
+    ],
+    image: "https://www.svgrepo.com/show/373554/django.svg",
+    source_code_link: "https://github.com/DevAhmedKhalil/storefrontAPI",
+  },
+  {
+    name: "Movie Explorer App",
+    description:
+      "A React-based application to explore movies, featuring popular lists, detailed views, search functionality, and pagination using the TMDb API.",
+    tags: [
+      { name: "React.js", color: "blue-text-gradient" },
+      { name: "TMDb API", color: "green-text-gradient" },
+      { name: "JavaScript", color: "pink-text-gradient" },
+    ],
+    image:
+      "https://miro.medium.com/v2/resize:fit:1400/1*x0d41ns8PTQZz4a3VbMrBg.png",
+    source_code_link: "https://github.com/DevAhmedKhalil/Movies-ReactApp",
+  },
+];
